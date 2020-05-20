7853fd06b8ee9994217cd7d17b2961bfa1f750e5
cipher <cipher@nervos.org>
2020-05-20 20:07:57 +0800

    fix the inflation rate bug

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # data in billion
-INITIAL_SUPPLY = 33.6 * (1 - 0.25)  # burn 25%
+INITIAL_SUPPLY = 33.6
 PRIMARY_SUPPLY_1STYEAR = 33.6 / 8
 SECONDARY_SUPPLY_YEARLY = 1.344
 HALFING_PERIOD = 4
@@ -48,12 +48,12 @@ def Inflation_Rate_Comparison(draw = True):
         plt.plot(t_monthly, APC, "g-.", linewidth=0.8, label = 'Nominal Compensation Rate')
         # Real Inflation Rate 实际通胀率：一级增发除以总发行
         plt.plot(t_monthly, real_inflation, "b", label = 'Real Inflation Rate')
-        plt.ylim((0, 25))
+        plt.ylim((0, 20))
         plt.xlim((0, cut_off_year))
         plt.xticks(np.arange(0, cut_off_year, 4))
         plt.legend(loc='upper right')
         plt.xlabel('Time / year')
-        plt.ylabel("Interest / %")
+        plt.ylabel("Inflation rate / %")
         plt.show()
     return (inflation_rate, APC, real_inflation)
 
@@ -62,7 +62,7 @@ def Inflation_Rate_Comparison(draw = True):
     等效硬顶曲线:理论总发行量折算到创世块的等效值
 '''
 def deFacto_Hard_Cap(draw = True):
-    primary_hard_cap = INITIAL_SUPPLY + PRIMARY_SUPPLY_1STYEAR*4*2
+    primary_hard_cap = INITIAL_SUPPLY*0.75 + PRIMARY_SUPPLY_1STYEAR*4*2
     t_monthly = np.arange(0, cut_off_year, 1./12)
     if draw:
         plt.plot((t_monthly[0], t_monthly[-1]), (primary_hard_cap, primary_hard_cap), 
@@ -77,7 +77,8 @@ def deFacto_Hard_Cap(draw = True):
         accumulated_interest = 1.0
         index = 0
         for supply in total_supply_monthly:
-            deFactoSupply.append(supply / accumulated_interest)
+            supply_as_genesis = supply / accumulated_interest - INITIAL_SUPPLY*0.25
+            deFactoSupply.append(supply_as_genesis)
             accumulated_interest *= (1 + monthly_apc[index])
             index += 1
         deFactoSupply_array.append(deFactoSupply)
